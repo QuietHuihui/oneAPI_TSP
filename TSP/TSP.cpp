ba265ea1223b059719c708fbc1e06163dcf5667a
QuietHuihui <huige02@outlook.com>
2023-01-15 20:13:22 +0800

    完成了评估函数的编写

--- TSP/TSP.cpp
+++ TSP/TSP.cpp
@@ -19,7 +19,8 @@ class TSP {
 public:
 
 	TSP() {
-		this->solution = vector<int>{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+		//初始化解决方案：到达各个城市的顺序
+		this->solution = vector<int>{0,1,2,4,3,5,6,7,8,9,10,11,12,13,14};
 	}
 
 	//城市坐标
@@ -27,6 +28,7 @@ public:
 	//城市序列
 	vector<int>solution;
 
+	//初始化城市，随机生成坐标
 	void initCity() {
 			vector<pair<int, int>>cities(CITY_NUM);
 			sycl::buffer<pair<int, int>>a{ cities };
@@ -49,6 +51,7 @@ public:
 			this->city = cities;
 	}
 
+	//展示随机生成的城市坐标
 	void showCity() {
 		cout << "生成随机城市的坐标: " << endl;
 		for (int i = 0; i < this->city.size(); i++) {
@@ -57,27 +60,15 @@ public:
 
 	}
 
-	void calDistance() {
-
-		//基本思路
-		//先并行地把x和y坐标各自相减
-		//然后给x和y坐标相减的结果各自平方
-		//平方的结果，对应下标的相加到一个新的vector里
-		//把新vector里的数并行地开根号
-		//把这些开根的结果全部加起来，返回
+	//评估函数，计算当前解决方案的距离之和
+	int calDistance() {
 
 		sycl::queue q;
-		int result;
 
 		//把solution后移一位得到用来相减的vector
 		vector<int>r_solution(this->solution.begin()+1,this->solution.end());
 		r_solution.push_back(this->solution[0]);
 
-		//第一步，先将solution和r_solution的内容相减
-		//存放x坐标相减结果的vector
-		vector<int>sub_parallel_x(CITY_NUM);
-		//存放y坐标相减结果的vector
-		vector<int>sub_parallel_y(CITY_NUM);
 		//存放((x1-x2)^2+(y1-y2)^2)^0.5的vector
 		vector<int>sub_vec(CITY_NUM);
 
@@ -85,23 +76,17 @@ public:
 		sycl::buffer a_buf(this->solution);
 		sycl::buffer b_buf(r_solution);
 		sycl::buffer city_buf(this->city);
-		//sycl::buffer sub_buf_x(sub_parallel_x);
-		//sycl::buffer sub_buf_y(sub_parallel_y);
 		sycl::buffer sub_buf(sub_vec);
 
-		//进行并行减
+		//并行地计算距离之和
 		for (size_t i = 0; i < CITY_NUM; i++) {
 			q.submit([&](sycl::handler& h) {
 				sycl::accessor a(a_buf, h, sycl::read_only);
 				sycl::accessor b(b_buf, h, sycl::read_only);
 				sycl::accessor cty(city_buf, h, sycl::read_only);
-				//sycl::accessor sub_x(sub_buf_x, h, sycl::write_only, sycl::no_init);
-				//sycl::accessor sub_y(sub_buf_y, h, sycl::write_only, sycl::no_init);
 				sycl::accessor sub(sub_buf, h, sycl::write_only, sycl::no_init);
 
 				h.parallel_for(CITY_NUM, [=](auto i) {
-					//sub_x[i] = pow(cty[a[i]].first - cty[b[i]].first,2);
-					//sub_y[i] = pow(cty[a[i]].second - cty[b[i]].second,2);
 					sub[i] = pow(pow(cty[a[i]].first - cty[b[i]].first, 2) + pow(cty[a[i]].second - cty[b[i]].second, 2), 0.5);
 					});
 
@@ -109,9 +94,17 @@ public:
 		};
 		q.wait();
 
+		//输出查看每一个点之间的距离
+		//cout << "计算执行成功" << endl;
+		//for (int i = 0; i < CITY_NUM; i++)
+		//	cout << sub_vec[i] << endl;
 
-
-		cout << "计算执行成功" << endl;
+		//把并行计算得到的每一个点的距离加起来，得到当前solution的评估值
+		int result = 0;
+		for (int i = 0; i < CITY_NUM; i++)
+			result += sub_vec[i];
+		cout << "当前评估值是: " << result << endl;
+		return result;
 	}
 };
 int main() {
